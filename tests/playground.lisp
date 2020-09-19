649e8b07b4fc8ad10f04a69c629bd16a61309f8a
vindarel <vindarel@mailz.org>
2020-09-19 02:12:56 +0200

    enhance if…progn rule, don't match when there's an else clause.
    so we can run this rule on good code and see no false positive.

--- tests/playground.lisp
+++ tests/playground.lisp
@@ -1,5 +1,4 @@
 
-
 ;; Transform if… progn to when.
 (if (and (getf options :version)
              (foo)
@@ -12,6 +11,15 @@
       (format t "Project version ~a~&" +version+)
       (print-system-info)
       (uiop:quit)))
+
+
+
+;; Do NOT transform the if here.
+(if (true)
+    (progn ;; should NOT be removed.
+      (then-1)
+      (then-2))
+    (else))
 
 ;; Remove print.
 (push (hunchentoot:create-folder-dispatcher-and-handler
